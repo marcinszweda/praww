3ce15a336cfde5c72b0b4bc683815ecd7381d89c
admin <admin@10.14.3.114>
2015-06-11 13:34:49 +0200

    77

---
+++ src/com/mycompany/prawwn/zad111/TikTaki.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prawwn.zad111;
+
+/**
+ *
+ * @author admin
+ */
+public class TikTaki {
+    //get free version
+    //http://www.pcsteps.com/45-download-windows-7-iso-legally-free/
+    
+    //co i na czym bylo
+    //http://support.lenovo.com/us/en/documents/migr-70528#rohs
+    
+}
